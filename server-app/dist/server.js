824171b0b8fd3d8dbbb17925661647203feb174e
rahat728 <asifuzzamanrahat728@gmail.com>
2025-02-03 23:09:27 +0600

    server.js updated

--- server-app/dist/server.js
+++ server-app/dist/server.js
@@ -15,7 +15,7 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cors_1.default)({
-    origin: "http://localhost:3000",
+    origin: "https://link-shortener-frontend.onrender.com/",
     credentials: true,
 }));
 app.use("/api/", shrtUrl_1.default);
